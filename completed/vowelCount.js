d44f05d5cf96c1efe9804576c681461644d99435
Louis Coleman <coleloui18@gmail.com>
2021-04-11 11:21:39 -0700

    vowel count completed

--- completed/vowelCount.js
+++ completed/vowelCount.js
@@ -3,8 +3,14 @@
 // TODO:The input string will only consist of lower case letters and/or spaces.
 function getCount(str) {
     var vowelsCount = 0;
-
     // enter your majic here
-
+  let a = str.split("")
+  a.map(letter => {
+    if(/([a,e,i,o,u])/.test(letter) === true){
+    vowelsCount++
+    }
+  });
     return vowelsCount;
   }
+
+  getCount("abracadabra")
