e8aa2f50ea9f8fbad30038c11f082f387add67d9
Louis Coleman <coleloui18@gmail.com>
2021-04-11 10:52:17 -0700

    start vowels

---
+++ vowelCount.js
@@ -0,0 +1,10 @@
+// TODO:Return the number (count) of vowels in the given string.
+// TODO:We will consider a, e, i, o, u as vowels for this Kata (but not y).
+// TODO:The input string will only consist of lower case letters and/or spaces.
+function getCount(str) {
+    var vowelsCount = 0;
+
+    // enter your majic here
+
+    return vowelsCount;
+  }
