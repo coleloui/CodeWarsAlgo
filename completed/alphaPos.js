cebe96c70ca769983ae3e06a55fe7964b7f57fe4
Louis Coleman <coleloui18@gmail.com>
2021-05-08 07:40:07 -0700

    addition

--- completed/alphaPos.js
+++ completed/alphaPos.js
@@ -1,6 +1,6 @@
 // TODO: In this kata you are required to, given a string, replace every letter with its position in the alphabet.
-// TODO:If anything in the text isn't a letter, ignore it and don't return it.
-// TODO:"a" = 1, "b" = 2, etc.
+// TODO: If anything in the text isn't a letter, ignore it and don't return it.
+// TODO: "a" = 1, "b" = 2, etc.
 
 function alphabetPosition(text) {
     return text.toLowerCase().split('')
